aa07afcaaad7010ef1a3edeab3e4335271b27bed
northantsvintage <northantsvintage@gmail.com>
2020-08-25 11:51:06 +0100

    methods-add, lower, reset and this.setState

--- src/Book.js
+++ src/Book.js
@@ -5,14 +5,39 @@ export default class Book extends Component {
     super(props);
     this.state = {
       count: 1,
+      name: "John",
     };
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    console.log("you clicked me");
-    console.log(this.state.count);
-  }
+  // handleClick() {
+  //   console.log("you clicked me");
+  //   console.log(this.state.count);
+  // }
+
+  // arrow function doesen't need binding
+  // handleClick = () => {
+  //   console.log("clicked me");
+  //   console.log(this.state.count);
+  // };
+
+  addCount = () => {
+    this.setState({
+      count: this.state.count + 1,
+      name: "Peter",
+    });
+  };
+  lowerCount = () => {
+    this.setState({
+      count: this.state.count - 1,
+      name: "Jack",
+    });
+  };
+  resetCount = () => {
+    this.setState({
+      count: 0,
+    });
+  };
 
   render() {
     // console.log(this.props);
@@ -24,8 +49,16 @@ export default class Book extends Component {
         <div>
           <h3>Title: {title} </h3>
           <h4>Author: {author} </h4>
-          <button type="button" onClick={this.handleClick}>
+          <h5>count: {this.state.count}</h5>
+          <h6>name: {this.state.name}</h6>
+          <button type="button" onClick={this.addCount}>
             add count
+          </button>
+          <button type="button" onClick={this.lowerCount}>
+            lower count
+          </button>
+          <button type="button" onClick={this.resetCount}>
+            reset count
           </button>
         </div>
       </article>
